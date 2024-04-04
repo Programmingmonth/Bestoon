5d3a950b1f00ef0557920c519bb67b223dcd5ea1
Williamfox2012 <141017859+Programmingmonth@users.noreply.github.com>
2024-04-04 12:46:13 +0330

    token

--- bestoon/web/models.py
+++ bestoon/web/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+
+class Token(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __unicode__(self):
+        return "{}_token".format(self.user)
+
+
 # Create your models here.
 class Expense(models.Model):
     text = models.CharField(max_length=255)
--- bestoon/web/views.py
+++ bestoon/web/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User, Token, Expense
 # Create your views here.
+
+@csrf_exempt
 
 def submit_expense(request):
     """user submits an expense"""
-    print("we are here")
+
+    this_token = request.Post['token']
+    this_user = User.objects.filter(token__token = this_user).get()
+    Expense.objects.create(user = this_user, amount=request.POST['amount']
+            text = request.POST['text'], date=now)
+    print ("I'm in submit expense")
+    print (request.POST)
+
+
+    return JsonResponse({
+        'status': 'ok' 
+    }, encoder=JSONEncoder)
